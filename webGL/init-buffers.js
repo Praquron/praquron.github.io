75145f7d49fac60f4fd63fdbe24d576e7d52367b
Praquron <118047774+Praquron@users.noreply.github.com>
2022-12-13 13:39:20 +0800

    Update init-buffers.js

--- webGL/init-buffers.js
+++ webGL/init-buffers.js
@@ -1,8 +1,10 @@
 function initBuffers(gl) {
   	const positionBuffer = initPositionBuffer(gl);
-
+	const colorBuffer = initColorBuffer(gl);
+	
   	return {
     	position: positionBuffer,
+		color: colorBuffer,
   	};
 }
 
@@ -23,6 +25,16 @@ function initPositionBuffer(gl) {
   	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   	return positionBuffer;
+}
+
+function initColorBuffer(gl) {
+  	const colors = [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0];
+  
+  	const colorBuffer = gl.createBuffer();
+  	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  
+  	return colorBuffer;
 }
 
 export { initBuffers };
