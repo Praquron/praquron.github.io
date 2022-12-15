3a2c3776c4b6d4682227eb7ba1c7568125301183
Praquron <118047774+Praquron@users.noreply.github.com>
2022-12-15 17:07:41 +0800

    Update webgl-demo.js

--- webGL/webgl-demo.js
+++ webGL/webgl-demo.js
@@ -177,3 +177,30 @@ function loadTexture(gl, url) {
 	);
 	
 	const image = new Image();
+	image.onload = () => {
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.textImage2D(
+			gl.TEXTURE_2D,
+			level,
+			internalFormat,
+			srcFormat,
+			srcType,
+			image,
+		);
+		
+		if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+			gl.generateMipmap(gl.TEXTURE_2D);
+		} else {
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		}
+	};
+	image.src = url;
+	
+	return texture;
+}
+
+function isPowerOf2(value) {
+	return (value & (value - 1)) === 0;
+}
