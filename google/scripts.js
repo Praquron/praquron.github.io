2319916372ce146fdbd3694649ccc929708e3044
Praquron <praquron@gmail.com>
2022-12-22 16:16:15 +0800

    Update scripts.js

--- google/scripts.js
+++ google/scripts.js
@@ -12,49 +12,52 @@ for (let i = 0; i < imageSource.length; i++) {
 load(0);
 
 const previousDoodleButton = document.querySelector("button.previousDoodle");
-previousDoodleButton.addEventListener('click', function() {
+previousDoodleButton.addEventListener("click", function () {
 	load(-1);
 });
 const nextDoodleButton = document.querySelector("button.nextDoodle");
-nextDoodleButton.addEventListener('click', function() {
+nextDoodleButton.addEventListener("click", function () {
 	load(1);
 });
 const loadDoodleButton = document.querySelector("button.loadDoodle");
-loadDoodleButton.addEventListener('click', function() {
-	load(0);
+loadDoodleButton.addEventListener("click", function () {
+	load(1);
+	load(-1);
 });
 
 const previousImageButton = document.querySelector("button.previousImage");
-previousImageButton.addEventListener('click', function() {
+previousImageButton.addEventListener("click", function () {
 	crop(-1);
 });
 const nextImageButton = document.querySelector("button.nextImage");
-nextImageButton.addEventListener('click', function() {
+nextImageButton.addEventListener("click", function () {
 	crop(1);
 });
 const loadImageButton = document.querySelector("button.loadImage");
-loadImageButton.addEventListener('click', function() {
-	crop(0);
+loadImageButton.addEventListener("click", function () {
+	crop(1);
+	crop(-1);
 });
 const downloadImageButton = document.querySelector("button.downloadImage");
-downloadImageButton.addEventListener('click', function() {
+downloadImageButton.addEventListener("click", function () {
 	downloadImage();
 });
 
 const previousAudioButton = document.querySelector("button.previousAudio");
-previousAudioButton.addEventListener('click', function() {
+previousAudioButton.addEventListener("click", function () {
 	trim(-1);
 });
 const nextAudioButton = document.querySelector("button.nextAudio");
-nextAudioButton.addEventListener('click', function() {
+nextAudioButton.addEventListener("click", function () {
 	trim(1);
 });
 const loadAudioButton = document.querySelector("button.loadAudio");
-loadAudioButton.addEventListener('click', function() {
-	trim(0);
+loadAudioButton.addEventListener("click", function () {
+	trim(1);
+	trim(-1);
 });
 const downloadAudioButton = document.querySelector("button.downloadAudio");
-downloadAudioButton.addEventListener('click', function() {
+downloadAudioButton.addEventListener("click", function () {
 	downloadAudio();
 });
 
@@ -62,12 +65,11 @@ function checkDoodleI(change) {
 	if (typeof doodleI !== "number") {
 		doodleI = 0;
 	}
-	
 	doodleI += change;
-	
+
 	if (doodleI < 0) {
 		doodleI = 0;
-	} else if (doodleI > (imageSource.length - 1)) {
+	} else if (doodleI > imageSource.length - 1) {
 		doodleI = imageSource.length - 1;
 	}
 }
@@ -75,12 +77,12 @@ function checkImageI(change) {
 	if (typeof imageI !== "number") {
 		imageI = 0;
 	}
-	
+
 	imageI += change;
-	
+
 	if (imageI < 0) {
 		imageI = 0;
-	} else if (imageI > (imageProperties[doodleI].length - 1)) {
+	} else if (imageI > imageProperties[doodleI].length - 1) {
 		imageI = imageProperties[doodleI].length - 1;
 	}
 }
@@ -88,23 +90,26 @@ function checkAudioI(change) {
 	if (typeof audioI !== "number") {
 		audioI = 0;
 	}
-	
+
 	audioI += change;
-	
+
 	if (audioI < 0) {
 		audioI = 0;
-	} else if (audioI > (audioProperties[doodleI].length - 1)) {
+	} else if (audioI > audioProperties[doodleI].length - 1) {
 		audioI = audioProperties[doodleI].length - 1;
 	}
 }
 
 function load(change) {
-	doodleI = (Math.floor(document.getElementById("doodleIndex").value)) - 1;
+	doodleI = Math.floor(document.getElementById("doodleIndex").value) - 1;
+	let lastI = doodleI;
 	checkDoodleI(change);
 	document.getElementById("doodleIndex").value = doodleI + 1;
-	
-	crop(0);
-	trim(0);
+
+	if (doodleI !== lastI) {
+		crop(0);
+		trim(0);
+	}
 
 	document.getElementById("doodleSourceAnchor").href = doodleSource[doodleI];
 	document.getElementById("doodleSource").innerHTML = doodleName[doodleI];
@@ -116,23 +121,20 @@ function crop(change) {
 		if (document.getElementById("imageIndex").value === 0) {
 			imageI = lastImageI[doodleI];
 		} else {
-			imageI = (Math.floor(document.getElementById("imageIndex").value)) - 1;
+			imageI = Math.floor(document.getElementById("imageIndex").value) - 1;
 		}
-		checkImageI(change);
-		
-		if (imageI !== lastImageI[doodleI]) {
-			imageI = lastImageI[doodleI];
-			document.getElementById("imageIndex").value = imageI + 1;
-			cropImage(imageSource[doodleI][imageProperties[doodleI][imageI][0]], imageProperties[doodleI][imageI][1], imageProperties[doodleI][imageI][2], imageProperties[doodleI][imageI][3], imageProperties[doodleI][imageI][4], imageOriginalSource[doodleI][imageProperties[doodleI][imageI][0]]);
-		}
-		
-		lastImageI[doodleI] = imageI;
-		document.getElementById("imageIndex").value = imageI + 1;
-	} else {
-		cropImage("./assets/main/empty.png", 0, 0, 0, 0, "Not Avaliable");
-		
+	let lastI = imageI;
+	checkImageI(change);
+	lastImageI[doodleI] = imageI;
+	document.getElementById("imageIndex").value = imageI + 1;
+
+	if (imageI === lastI) {
 		lastImageI[doodleI] = "Not Avaliable";
 		document.getElementById("imageIndex").value = 0;
+
+		cropImage("./assets/main/empty.png", 0, 0, 0, 0, "Not Avaliable");
+	} else {
+		cropImage(imageSource[doodleI][imageProperties[doodleI][imageI][0]], imageProperties[doodleI][imageI][1], imageProperties[doodleI][imageI][2], imageProperties[doodleI][imageI][3], imageProperties[doodleI][imageI][4], imageOriginalSource[doodleI][imageProperties[doodleI][imageI][0]]);
 	}
 }
 function trim(change) {
@@ -140,54 +142,53 @@ function trim(change) {
 		if (document.getElementById("audioIndex").value === 0) {
 			audioI = lastAudioI[doodleI];
 		} else {
-			audioI = (Math.floor(document.getElementById("audioIndex").value)) - 1;
+			audioI = Math.floor(document.getElementById("audioIndex").value) - 1;
 		}
-		checkAudioI(change);
-		
-		if (audioI !== lastAudioI[doodleI]) {
-			audioI = lastAudioI[doodleI];
-			document.getElementById("audioIndex").value = audioI + 1;
-			trimAudio(audioSource[doodleI][audioProperties[doodleI][audioI][0]], audioProperties[doodleI][audioI][1], audioProperties[doodleI][audioI][2], audioOriginalSource[doodleI][audioProperties[doodleI][audioI][0]]);
-		}
-		
-		lastAudioI[doodleI] = audioI;
-		document.getElementById("audioIndex").value = audioI + 1;
-	} else {
-		trimAudio("./assets/main/empty.mp3", 0, 0, "Not Avaliable");
-		
+	}
+	let lastI = audioI;
+	checkAudioI(change);
+	lastAudioI[doodleI] = audioI;
+	document.getElementById("audioIndex").value = audioI + 1;
+
+	if (audioI === lastI) {
 		lastAudioI[doodleI] = "Not Avaliable";
 		document.getElementById("audioIndex").value = 0;
+
+		trimAudio("./assets/main/empty.mp3", 0, 0, "Not Avaliable");
+	} else {
+		trimAudio(audioSource[doodleI][audioProperties[doodleI][audioI][0]], audioProperties[doodleI][audioI][1], audioProperties[doodleI][audioI][2], audioOriginalSource[doodleI][audioProperties[doodleI][audioI][0]]);
 	}
 }
 
 function cropImage(imagePath, newX, newY, newWidth, newHeight, originalImagePath) {
-	const canvas = document.getElementById('image');
-	let ctx = canvas.getContext('2d');
-	
+	const canvas = document.getElementById("image");
+	let ctx = canvas.getContext("2d");
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	let originalImage = new Image();
 	originalImage.src = imagePath;
-	ctx = canvas.getContext('2d');
-			
-  	originalImage.addEventListener('load', function() {
-  		canvas.width = newWidth;
-		canvas.height = newHeight;
-		ctx.drawImage(originalImage, newX, newY, newWidth, newHeight, 0, 0, newWidth, newHeight);
-				
-		document.getElementById("imageWidth").innerHTML = newWidth;
-		document.getElementById("imageHeight").innerHTML = newHeight;
-		document.getElementById("imageX").innerHTML = newX;
-		document.getElementById("imageY").innerHTML = newY;
-		document.getElementById("imageSourceAnchor").href = originalImagePath;
-		document.getElementById("imageSource").innerHTML = originalImagePath;
-  	});
+	ctx = canvas.getContext("2d");
+
+	originalImage.addEventListener("load", function () {
+	canvas.width = newWidth;
+	canvas.height = newHeight;
+	ctx.drawImage(originalImage, newX, newY, newWidth, newHeight, 0, 0, newWidth, newHeight);
+
+	document.getElementById("imageWidth").innerHTML = newWidth;
+	document.getElementById("imageHeight").innerHTML = newHeight;
+	document.getElementById("imageX").innerHTML = newX;
+	document.getElementById("imageY").innerHTML = newY;
+	document.getElementById("imageSourceAnchor").href = originalImagePath;
+	document.getElementById("imageSource").innerHTML = originalImagePath;
+	});
 }
 function trimAudio(audioPath, audioStart, audioEnd, originalAudioPath) {
-	document.getElementById("audio").src = audioPath + "#t=" + audioStart + "," + audioEnd;
-   	
+	document.getElementById("audio").src =
+	audioPath + "#t=" + audioStart + "," + audioEnd;
+
 	document.getElementById("audioStart").innerHTML = audioStart;
 	document.getElementById("audioEnd").innerHTML = audioEnd;
-	document.getElementById("audioDuration").innerHTML = audioEnd - audioStart; 
+	document.getElementById("audioDuration").innerHTML = audioEnd - audioStart;
 	document.getElementById("audioSourceAnchor").href = originalAudioPath;
 	document.getElementById("audioSource").innerHTML = originalAudioPath;
 }
@@ -195,16 +196,16 @@ function trimAudio(audioPath, audioStart, audioEnd, originalAudioPath) {
 function downloadImage() {
 	var element = document.getElementById("image");
 	var elementDownload = element.toDataURL("image/png").replace("image/png", "image/octet-stream");
-	
-	var download = document.createElement('a');
+
+	var download = document.createElement("a");
 	download.download = "imageIndex(" + imageI + ").png";
 	download.href = elementDownload;
 	download.click();
 }
 function downloadAudio() {
 	var element = document.getElementById("audio");
-	
-	var download = document.createElement('a');
+
+	var download = document.createElement("a");
 	download.download = "audioIndex(" + audioI + ").mp3";
 	download.href = element.src + "#t=" + audioProperties[doodleI][audioI][1] + "," + audioProperties[doodleI][audioI][2];
 	download.click();
