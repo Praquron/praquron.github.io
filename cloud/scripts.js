3967c6823131ec138466c1f00f097e393b379abe
Praquron <praquron@gmail.com>
2022-12-27 00:28:07 +0800

    Update scripts.js

--- cloud/scripts.js
+++ cloud/scripts.js
@@ -1,11 +1,7 @@
 document.head.appendChild((($, s)=>{$.src=s;return $})(document.createElement("script"), "https://kihtrak.com/cloud_variable/cloudify.min.js"));
 
-let cloud_console;
+let cloud_console = 0;
 
-if (typeof cloud_console == "number") {
-    cloud_console++;
-} else {
-    cloud_console = 0;
-}
+cloud_console++;
 
 document.getElementById("console").innerHTML = cloud_console;
