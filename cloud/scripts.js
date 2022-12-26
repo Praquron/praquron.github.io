9c2e6c20bb159e4ca26a69325e0dd6eeedf90756
Praquron <praquron@gmail.com>
2022-12-27 00:24:30 +0800

    Update scripts.js

--- cloud/scripts.js
+++ cloud/scripts.js
@@ -1,3 +1,5 @@
+document.head.appendChild((($, s)=>{$.src=s;return $})(document.createElement("script"), "https://kihtrak.com/cloud_variable/cloudify.min.js"));
+
 let cloud_console;
 
 if (typeof cloud_console == "number") {
