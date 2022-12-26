80583c38aea6aacbb268d4a24a5505ff4beccfea
Praquron <praquron@gmail.com>
2022-12-26 21:57:22 +0800

    Update scripts.js

--- cloud/scripts.js
+++ cloud/scripts.js
@@ -1,5 +1,9 @@
 var cloud_console;
 
-cloud_console++;
+if (cloud_console == undefined) {
+    cloud_console++;
+} else {
+    cloud_console = 0;
+}
 
 document.getElementById("console").innerHTML = cloud_console;
