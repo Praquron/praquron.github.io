baff77c6553ca1fdfc0d9ff26fde84f13dafce5e
Praquron <praquron@gmail.com>
2022-12-28 08:01:50 +0800

    Delete cloud directory

--- cloud/README.md
+++
@@ -1,3 +0,0 @@
-# [cloud](https://praquron.github.io/cloud)
-
-This is an experiment of testing cloud variables using [Kihtrak](https://kihtrak.com/cloud_variable/).
--- cloud/scripts.js
+++
@@ -1,6 +0,0 @@
-let cloud_console = 0;
-
-setInterval(function() {
-    cloud_console++;
-    document.getElementById("console").innerHTML = cloud_console;
-}, 1000/60);
